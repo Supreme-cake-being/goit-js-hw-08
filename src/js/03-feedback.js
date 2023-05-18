097b1f182593cf6984d8566ade874c8fccdd4278
supreme-cake-being <XGon20987@gmail.com>
2023-05-18 11:56:56 +0300

    done

---
+++ src/js/03-feedback.js
@@ -0,0 +1,34 @@
+import throttle from 'lodash.throttle';
+
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const form = document.querySelector("form");
+
+getFromLocalStorage();
+
+form.addEventListener("input", throttle((e) => {
+    const feedback = {
+        email: input.value,
+        message: textarea.value,
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+}, 500));
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    form.reset();
+    localStorage.removeItem("feedback-form-state");
+});
+
+function getFromLocalStorage() {
+    const feedback = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+    if (!feedback)
+        return;
+
+    input.value = feedback.email;
+    textarea.value = feedback.message;
+
+}
